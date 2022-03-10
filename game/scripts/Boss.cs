50dc55f6189f9295bacc7c2938b8dd563a7fec0c
garo_n <garo_n@etna-alternance.net>
2022-03-10 10:57:16 +0100

    [FEATURE]Hell bullet machine with capsuled pattern

--- game/scripts/Boss.cs
+++ game/scripts/Boss.cs
@@ -1,5 +1,5 @@
 using Godot;
-using System;	
+using System;
 
 public class Boss : Node2D
 {
@@ -8,50 +8,58 @@ public class Boss : Node2D
 	// private string b = "text";
 	private const int Rotate_speed = 100;
 	private const double Shooter_timer = 0.2;
-	private const int Spawn_points = 4;
+	private const int Spawn_points = 8;
 	private const int Radius = 10;
-	
+	PackedScene Bullet_scene = (PackedScene) ResourceLoader.Load("res://scenes/BossPat/Bullet.tscn");
+	private Node2D rotater = null;
+	private Timer shoot_timer = null;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var shoot_timer = GetNode<Timer>("ShootTimer");
-		var rotater = GetNode<Node2D>("Rotater");
-		
-		
+		shoot_timer = GetNode<Timer>("ShootTimer");
+		rotater = GetNode<Node2D>("Rotater");
 
-		const double Step = 2 * 3.14 / Spawn_points;
+
+		const double Step = 2 * Mathf.Pi / Spawn_points;
+
 		const int StepI = (int) Step;
-		
+
 		for (int i = 0; i < Spawn_points; i++)
 		{
 			var spawn = new Node2D();
-			var pos = new Vector2(Radius, 0).Rotated((float)StepI * i);
+			var pos = new Vector2(Radius, 0).Rotated((float) StepI * i);
 			spawn.Position = pos;
 			spawn.Rotation = pos.Angle();
 			rotater.AddChild(spawn);
-			
 		}
 
 		shoot_timer.WaitTime = (float) Shooter_timer;
 		shoot_timer.Start();
-		
 	}
-	
+
+	public override void _Process(float delta)
+	{
+		rotater = GetNode<Node2D>("Rotater");
+		circlePattern(rotater,delta);
+	}
+
 	private void _on_ShootTimer_timeout()
 	{
-		var rotater = GetNode<Node2D>("Rotater");
-		var Bullet_scene = (PackedScene) ResourceLoader.Load("res://scenes/BossPat/Bullet.tscn");
-
-		
+		rotater = GetNode<Node2D>("Rotater");
 		// Replace with function body.
-		foreach (var child in rotater.GetChildren())
+		foreach (Node2D child in rotater.GetChildren())
 		{
-			var bullet = Bullet_scene.Instance();
-			GetTree().Root.AddChild(bullet);
+			Node2D bullet = Bullet_scene.Instance<Node2D>();
+			AddChild(bullet);
+			bullet.Position = child.GlobalPosition;
+			bullet.Rotation = child.GlobalRotation;
 		}
 	}
 
-
+	private void circlePattern(Node2D rotater,float delta)
+	{
+		float new_rotation = rotater.RotationDegrees + Rotate_speed * delta;
+		rotater.RotationDegrees = new_rotation %360;
+	}
 }
-
-
